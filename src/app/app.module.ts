62a3c063b29ec433a35a0a5f63d772dd0e04d3b3
labstraction <labstraction@protonmail.com>
2026-02-17 10:24:11 +0100

    new component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './components/page-one/page-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent
   ],
   imports: [
     BrowserModule,
